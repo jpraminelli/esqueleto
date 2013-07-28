d25cd4049efb9336346d812623c611f0ff6d920a
joao <joao.paulo@redemagic.com>
2013-07-28 01:09:55 +0000

    ajustes

--- config/test.config.php
+++ config/test.config.php
@@ -1,12 +1,11 @@
 <?php
-
 return array(
-    'db' => array(
-        'driver' => 'PDO_SQLite',
-        'dsn' => 'sqlite::memory:',
-        'driver_options' => array(
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        )
-    )
-
-);
+ 'db' => array(
+      'driver' => 'PDO',
+      'dsn' => 'mysql:dbname=zf2napratica_test;host=localhost',
+      'username' => 'root',
+      'password' => '123456',
+      'driver_options' => array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+       ),
+ ));
